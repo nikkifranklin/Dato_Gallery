b13074a7f031a154a671bf97102f4c265228c27c
Nikki Franklin <nikkimfranklin@yahoo.com>
2014-06-10 15:06:54 -0400

    minimal changes

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer>
     
     	<div class="fbodytxt">
-        <p>address and phone number</p>
+        <p>1485 West Avenue St. Seattle, Washington 98103 | 206-228-9342</p>
         <p>Copyright 2014 Dato Gallery</p>
         <p>Disclaimer: This interactive media project was created by students for educational purposes at The Art Institute of Atlanta and is in no way intended for commercial gain or as a source of public information.</p>
         </div>
