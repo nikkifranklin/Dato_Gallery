22717d7b563f5c1e4918d7e9247f0aa05944d5da
Nikki Franklin <nikkimfranklin@yahoo.com>
2014-06-10 14:05:21 -0400

    added ext. php and css files
    .

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    
+    	<div class="fbodytxt">
+        <p>address and phone number</p>
+        <p>Copyright 2014 Dato Gallery</p>
+        <p>Disclaimer: This interactive media project was created by students for educational purposes at The Art Institute of Atlanta and is in no way intended for commercial gain or as a source of public information.</p>
+        </div>
+    
+</footer>
---
+++ includes/nav.php
@@ -0,0 +1,14 @@
+<header>
+    
+    <a href=""><img src="" alt="logo" /></a>
+    
+    	<nav>
+        	<ul>
+        	<li><a href="">About</a></li>
+            <li><a href="">Artists</a></li>
+            <li><a href="">Gallery</a></li>
+            <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+    
+</header>
